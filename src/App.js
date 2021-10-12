019f84019457e73258cc866a4d8620b0de535113
huydevct <huyvipct1@gmail.com>
2021-10-12 18:02:46 +0700

    styling and redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grid, Grow} from '@material-ui/core';
-
+import { useDispatch } from 'react-redux';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 
+import { getPosts } from './actions/posts'
 import memories from './images/memories.png';
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts())
+    }, [dispatch])
+
     return(
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memories" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography variant="h2" className={classes.heading} align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
