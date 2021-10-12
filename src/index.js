260b36813745a456fa74eb5426280e5b7d285549
huydevct <huyvipct1@gmail.com>
2021-10-12 22:44:56 +0700

    posts

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import thunk from "redux-thunk";
 
 import reducers  from "./reducers";
 
+import './index.css';
+
 import App from "./App.js";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
