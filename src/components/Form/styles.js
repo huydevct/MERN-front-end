499e31a4a0948f9095402b152374d265d86dbbee
huydevct <huyvipct1@gmail.com>
2021-10-12 22:05:26 +0700

    create post

--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -1,18 +1,24 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  appBar: {
-    borderRadius: 15,
-    margin: "30px 0",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
   },
-  heading: {
-    color: "rgba(0,183,255, 1)",
+  paper: {
+    padding: theme.spacing(2),
   },
-  image: {
-    marginLeft: "15px",
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  fileInput: {
+    width: '97%',
+    margin: '10px 0',
+  },
+  buttonSubmit: {
+    marginBottom: 10,
   },
 }));
